89a35379b9b6b4e207f8ecf813639a131f0ff336
xboudsady <boudsadyx@gmail.com>
2019-01-20 19:37:05 -0800

    Added ListingAdmin class in admin.py file to modifiy table listing view

--- listings/admin.py
+++ listings/admin.py
@@ -4,4 +4,8 @@ from .models import Listing
 
 # Register your models here.
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+
+admin.site.register(Listing, ListingAdmin)
