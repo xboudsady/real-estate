6cd9ca10e91dffbabc3cea255059366a42cf9ac3
xboudsady <boudsadyx@gmail.com>
2019-02-03 11:13:19 -0800

    Fixed context dictionary, added dynamic data to about.html for realtors section.

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 # Create your views here.
 
@@ -16,4 +17,18 @@ def index(request):
     return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    # dictionary to hold our method created above
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    # Get seller of month, has the mvp checkmark
+    # pass our context as 3rd parameter
+    return render(request, 'pages/about.html', context)
