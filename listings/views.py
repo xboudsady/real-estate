6bb1f031f03f74eb9e69b8933a9bc0268f25b192
xboudsady <boudsadyx@gmail.com>
2019-02-18 14:17:46 -0800

    Added keyword search, city search, state search

--- listings/views.py
+++ listings/views.py
@@ -31,6 +31,24 @@ def listing(request, listing_id):
 def search(request):
     queryset_list = Listing.objects.order_by('-list_date')
 
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryset_list = queryset_list.filter(state__iexact=state)
+
 
     context = {
         'state_choices': state_choices,
@@ -38,4 +56,5 @@ def search(request):
         'price_choices': price_choices,
         'listings': queryset_list
     }
+    
     return render(request, 'listings/search.html', context)
