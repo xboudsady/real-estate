bddd0b049b3d47fb68b04890d47284723c821097
xboudsady <boudsadyx@gmail.com>
2019-02-18 14:33:46 -0800

    Added search by price function

--- listings/views.py
+++ listings/views.py
@@ -54,6 +54,12 @@ def search(request):
         bedrooms = request.GET['bedrooms']
         if bedrooms:
             queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+    
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
 
 
     context = {
